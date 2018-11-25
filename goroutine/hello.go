9355d1035fe26cc82c33bd772312c05cdaa3d13f
niconico-pun <kon@kontany.net>
2018-11-25 15:58:47 +0900

    mutex sample

--- goroutine/hello.go
+++ goroutine/hello.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 	go sayhello()
+
+	var wg sync.WaitGroup
+	for _, salutation := range []string{"hello", "gretings", "good night"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
 }
 
 func sayhello() {
